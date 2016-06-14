0c05becd891c0baa5c03e0b6b78c57602195c12e
cirony <pereirasanchez.karinab@gmail.com>
2016-06-14 10:02:35 -0300

    apply-karina-pereira explanatory comments added for UniqueConstraintException class

--- src/main/java/org/jboss/as/quickstarts/kitchensink/exception/UniqueConstraintException.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/exception/UniqueConstraintException.java
@@ -8,11 +8,22 @@ public class UniqueConstraintException extends Exception{
 	
 	private Set<String> conflictiveParameters;
 	
+	/**
+	 * Creates a UniqueConstraintException. This kind of exception is meant to be used when 
+	 * trying to register a new Member through the web-form and the validation for the fields that 
+	 * must be unique in the database fails.
+	 * @param message Descriptive error message 
+	 * @param conflictiveParameters Name of the parameters that caused the conflict
+	 */
 	public UniqueConstraintException(String message, Set<String> conflictiveParameters){
 		super(message);
 		this.conflictiveParameters = conflictiveParameters;
 	}
 
+	/**
+	 * Obtain the list of names of the parameters that caused the UniqueConstraintException
+	 * @return conflictiveParameters Set with the name of the conflictive parameters
+	 */
 	public Set<String> getConflictiveParameters(){
 		return this.conflictiveParameters;
 	}
